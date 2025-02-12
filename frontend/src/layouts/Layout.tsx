0c72b2d8db8219f764fb975d2b00dc5cf10d02f1
Thabo Thobakgale <thobakgalethabo7@gmail.com>
2025-02-12 13:06:16 +0200

    layout error fixed

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,30 +1,11 @@
 import { Outlet } from "react-router-dom"
-import NavBar from "../components/navigation/NavBar"
-import SideBar from "../components/navigation/SideBar"
 
 
 function Layout() {
 
-	const routes = [
-		{
-			path: "/tasks",
-			name: "My Tasks"
-		},
-		{
-			path: "/addTask",
-			name: "Add Task"
-		},
-		{
-			path: "/editTasks",
-			name: "Edit Tasks"
-		}
-	]
-
   return (
     <>
-				<NavBar navList={routes} />
-				<SideBar navList={routes} />
-        <Outlet />
+		<Outlet />
     </>
   )
 }
