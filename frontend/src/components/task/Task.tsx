1384c901034b4181456e8ecc564346381d0e6537
Thabo Thobakgale <thobakgalethabo7@gmail.com>
2025-03-20 02:48:50 +0200

    tasks are now fetched from backend

--- frontend/src/components/task/Task.tsx
+++ frontend/src/components/task/Task.tsx
@@ -1,21 +1,56 @@
 import styles from './Task.module.css'
 
-function Task() {
+export type taskType ={
+  id:number,
+  task:string,
+  priority: "LOW" | "MEDIUM" | "HIGH",
+  dueDate: Date,
+  completed: boolean,
+}
+
+type propTypes = {
+  task:taskType
+}
+
+function formatTime(date: Date): string {
+  let hours = ""
+  let minutes = ""
+  if(date.getHours() < 10){
+    hours = "0" + date.getHours()
+  }
+  else {
+    hours += date.getHours()
+  }
+  if(date.getMinutes() < 10){
+    minutes = "0" + date.getMinutes()
+  }
+  else {
+    minutes += date.getMinutes()
+  }
+  return `${hours}:${minutes}`
+}
+
+function Task({ task }: propTypes) {
+  console.log(task)
   return (
     <li className={styles.container}>
       <div>
-        <input type="checkbox" name="" id=""/>
+        <input
+          type="checkbox"
+          // checked={task.completed}
+          name=""
+          id=""
+        />
       </div>
       <div className={styles.details}>
-        <div className={styles.time}>18:00</div>
-        <div className={styles.task}>Wash Car</div>
+        <div className={styles.time}>{formatTime(task.dueDate)}</div>
+        <div className={styles.task}>{task.task}</div>
       </div>
       <div className={styles.priority}>
-        High
+        {task.priority}
       </div>
 
     </li>
   );
 }
-
 export default Task;
--- frontend/src/components/tasklist/TaskList.tsx
+++ frontend/src/components/tasklist/TaskList.tsx
@@ -1,8 +1,45 @@
 import Task from "../task/Task.tsx";
 import styles from "./TaskList.module.css";
-
+import useCurrentList from "../../hooks/UseCurrentList.tsx";
+import {useEffect, useState} from "react";
+import {backendURL} from "../../AppVariables.ts";
+import useAuth from "../../hooks/UseAuth.tsx";
+import {taskType} from "../task/Task.tsx";
 
 function TaskList() {
+
+  const { listId } = useCurrentList()
+  const { userId, accessToken } = useAuth()
+
+  const [tasks, setTasks] = useState<taskType[]>([])
+
+  useEffect(()=>{
+    if (listId !== -1){
+      fetch(
+        `${backendURL}/api/task/all/${userId}/${listId}`,
+        {
+          method: "GET",
+          headers:{
+            authorization: `Bearer ${accessToken}`
+          }
+        }
+      ).then(res => res.json())
+        .then(data =>{
+          const newTasks: taskType[] = []
+          for (const item of data.tasks){
+            newTasks.push({
+              id: item.id,
+              task: item.task,
+              priority: item.priority,
+              dueDate: new Date(`${item.dueDate}T${item.dueTime}`),
+              completed: item.completed
+            })
+          }
+          setTasks(newTasks)
+        })
+    }
+  }, [listId])
+
   return (
     <div>
       <div className={styles.header}>
@@ -13,10 +50,19 @@ function TaskList() {
           <option value="priority">Priority</option>
         </select>
       </div>
-      <ul className={styles.list}>
-        <Task/>
-        <Task/>
-      </ul>
+      <>
+        {
+          tasks.length ?
+            <ul className={styles.list}>
+              {
+                tasks.map(
+                  (task => <Task key={task.id} task={task} />)
+                )
+              }
+            </ul> :
+            <p>No tasks found.</p>
+        }
+      </>
     </div>
   );
 }
