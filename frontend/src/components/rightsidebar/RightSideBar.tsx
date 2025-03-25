a8bdcd9700c4150be6d7b9d5895bcd14bc218e8a
Thabo Thobakgale <thobakgalethabo7@gmail.com>
2025-03-25 20:47:56 +0200

    logout functionality added

--- frontend/src/components/rightsidebar/RightSideBar.tsx
+++ frontend/src/components/rightsidebar/RightSideBar.tsx
@@ -2,10 +2,25 @@ import styles from './RightSideBar.module.css'
 import Calendar from "react-calendar";
 import './Calendar.css'
 import useCurrentDate from "../../hooks/UseCurrentDate.tsx";
+import {backendURL} from "../../AppVariables.ts";
+import {useNavigate} from "react-router-dom";
+import useAuth from "../../hooks/UseAuth.tsx";
 
 function RightSideBar() {
 
   const { currentDate, setCurrentDate } =useCurrentDate()!;
+  const navigate = useNavigate();
+  const { setAccessToken, setUserId } = useAuth()
+
+  function logout(){
+    fetch(`${backendURL}/auth/logout`, {
+      method: "GET",
+      credentials: "include",
+    })
+    navigate("../auth/login")
+    setAccessToken("")
+    setUserId("")
+  }
 
   return (
     <div className={styles.container}>
@@ -18,7 +33,7 @@ function RightSideBar() {
         />
       </div>
       <div className={styles.signOutBtnContainer}>
-        <button>Sign Out</button>
+        <button onClick={logout}>Log Out</button>
       </div>
     </div>
   );
